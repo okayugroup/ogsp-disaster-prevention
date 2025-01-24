429cd6e6257ba18f11b952fb4e51ab61c025d0e3
yossy4411 <yossy4411.dev@gmail.com>
2025-01-24 21:45:23 +0900

    テーマとフォントの実装
    メインビューにダークテーマをとりあえず適用し、フォントにはNoto Sans JPを使用しています。

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 #[derive(Default)]
-struct Counter {
-    value: i32,
+struct App {
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
 }
 
-use iced::widget::shader::Program;
 use iced::widget::{column, text, Column};
+use iced::{Settings, Size, Theme};
 
-impl Counter {
+impl App {
     pub fn view(&self) -> Column<Message> {
         // 縦に並べるやつ
         column![
@@ -20,9 +19,25 @@ impl Counter {
     pub fn update(&mut self, _message: Message) {
         // ここにイベント処理とかを書く。
     }
+    pub fn theme(&self) -> Theme {
+        Theme::Dark
+    }
+    pub fn settings() -> Settings {
+        let fonts = vec![
+            include_bytes!("../resources/fonts/NotoSansJP-Regular.ttf").as_slice().into(),
+            include_bytes!("../resources/fonts/NotoSansJP-Bold.ttf").as_slice().into(),
+        ];
+        Settings {
+            fonts,
+            ..Settings::default()
+        }
+    }
 }
 
 fn main() -> iced::Result {
-    iced::application("OGSP防災アプリ", Counter::update, Counter::view)
+    iced::application("OGSP Disaster Prevention", App::update, App::view)
+        .window_size(Size::new(800.0, 600.0))
+        .theme(App::theme)
+        .settings(App::settings())
         .run()
 }
